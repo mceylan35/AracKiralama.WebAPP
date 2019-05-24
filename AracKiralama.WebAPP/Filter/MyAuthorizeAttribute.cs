3531f0bf989ed1ad07e440bc69d5e6c47067c811
mceylan35 <mehmetceylan2696@gmail.com>
2019-05-24 11:40:21 +0300

    basic authorize

--- AracKiralama.WebAPP/Filter/MyAuthorizeAttribute.cs
+++ AracKiralama.WebAPP/Filter/MyAuthorizeAttribute.cs
@@ -8,35 +8,34 @@ using System.Threading;
 using System.Web;
 using System.Web.UI.WebControls;
 using System.Web;
-using System.Web.Http.Controllers;
-using System.Web.Http.Filters;
+
 namespace AracKiralama.WebAPP.Filter
 {
-    public class MyAuthorizeAttribute: AuthorizationFilterAttribute
+    public class MyAuthorizeAttribute//: AuthorizationFilterAttribute
     {
-        public override void OnAuthorization(HttpActionContext actionContext)
-        {
-            if (actionContext.Request.Headers.Authorization == null)
-            {
-                actionContext.Response = actionContext.Request.CreateResponse(System.Net.HttpStatusCode.Unauthorized);
-            }
-            else
-            {
-                var tokenKey = actionContext.Request.Headers.Authorization.Parameter;
-                //fgurdal:123456
-                var userNamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(tokenKey));
-                var userInfoArray = userNamePassword.Split(':');
-                var userName = userInfoArray[0];
-                var password = userInfoArray[1];
-                if (Login.UserNameAndPassword(userName, password))
-                {
-                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(userName), null);
-                }
-                else
-                {
-                    actionContext.Response = actionContext.Request.CreateResponse(System.Net.HttpStatusCode.Unauthorized);
-                }
-            }
-        }
+        //public override void OnAuthorization(HttpActionContext actionContext)
+        //{
+        //    if (actionContext.Request.Headers.Authorization == null)
+        //    {
+        //        actionContext.Response = actionContext.Request.CreateResponse(System.Net.HttpStatusCode.Unauthorized);
+        //    }
+        //    else
+        //    {
+        //        var tokenKey = actionContext.Request.Headers.Authorization.Parameter;
+        //        //fgurdal:123456
+        //        var userNamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(tokenKey));
+        //        var userInfoArray = userNamePassword.Split(':');
+        //        var userName = userInfoArray[0];
+        //        var password = userInfoArray[1];
+        //        if (Login.UserNameAndPassword(userName, password))
+        //        {
+        //            Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(userName), null);
+        //        }
+        //        else
+        //        {
+        //            actionContext.Response = actionContext.Request.CreateResponse(System.Net.HttpStatusCode.Unauthorized);
+        //        }
+        //    }
+        //}
     }
 }
